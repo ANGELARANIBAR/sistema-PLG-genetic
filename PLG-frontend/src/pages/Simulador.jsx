bb2514bdc296d29f401992fce05ca0172b3d863d
ANGELARANIBAR <163078398+ANGELARANIBAR@users.noreply.github.com>
2025-07-21 22:37:16 -0500

    version++

--- PLG-frontend/src/pages/Simulador.jsx
+++ PLG-frontend/src/pages/Simulador.jsx
@@ -250,7 +250,7 @@ export default function Simulador() {
                 setCurrentTime(prevTime => {
                     if (!prevTime) return new Date();
                     // Use seconds instead of minutes for smoother movement
-                    return new Date(prevTime.getTime() + 65000 * playbackSpeed);
+                    return new Date(prevTime.getTime() + 865000 * playbackSpeed);
                 });
             }, 1000);
         }
