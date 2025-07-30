38f1d79ee8915cdae0f59e90d60643724ee2ac6f
Piero Franchesco Gonzales Fernandez <pierogonzales+belcorp@belcorp.biz>
2025-07-29 21:11:04 -0500

    fixing pedidos

--- src/main/java/com/plg/planificacionplg/clases/Pedido.java
+++ src/main/java/com/plg/planificacionplg/clases/Pedido.java
@@ -32,7 +32,7 @@ public class Pedido{
     @Column(name = "volumen_glp_entregado", nullable = false, columnDefinition = "DOUBLE DEFAULT 0.0")
     private double volumenGLPEntregado;
     
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "ubicacion_id", referencedColumnName = "id")
     private Nodo ubicacion;
     
